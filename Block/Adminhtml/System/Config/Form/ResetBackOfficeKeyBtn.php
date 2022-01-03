5e0a3379214b00b9195047c369aa2ebe75a036d0
Ifthenpay <ricardocarvalho@ifthenpay.com>
2022-01-03 15:32:17 +0000

    change file resetBackofficeKey

--- Block/Adminhtml/System/Config/Form/ResetBackOfficeKeyBtn.php
+++ Block/Adminhtml/System/Config/Form/ResetBackOfficeKeyBtn.php
@@ -6,6 +6,7 @@
 * @package     Ifthenpay_Payment
 * @author      Ifthenpay
 * @copyright   Ifthenpay (http://www.ifthenpay.com)
+* @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
 */
 
 namespace Ifthenpay\Payment\Block\Adminhtml\System\Config\Form;
