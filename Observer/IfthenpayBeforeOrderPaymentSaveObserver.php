918bb8c9cd169a2ef22105e983b8c34b1f4e1357
josesoaresif <josesoares@ifthenpay.com>
2022-03-10 11:03:28 +0000

    fix bug that caused conflic with third party payment modules

--- Observer/IfthenpayBeforeOrderPaymentSaveObserver.php
+++ Observer/IfthenpayBeforeOrderPaymentSaveObserver.php
@@ -1,13 +1,14 @@
 <?php
+
 /**
-* Ifthenpay_Payment module dependency
-*
-* @category    Gateway Payment
-* @package     Ifthenpay_Payment
-* @author      Ifthenpay
-* @copyright   Ifthenpay (http://www.ifthenpay.com)
-* @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
-*/
+ * Ifthenpay_Payment module dependency
+ *
+ * @category    Gateway Payment
+ * @package     Ifthenpay_Payment
+ * @author      Ifthenpay
+ * @copyright   Ifthenpay (http://www.ifthenpay.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 
 declare(strict_types=1);
 
@@ -27,28 +28,35 @@ class IfthenpayBeforeOrderPaymentSaveObserver implements ObserverInterface
     private $logger;
     private $repositoryFactory;
 
-	public function __construct(
+    public function __construct(
         Gateway $gateway,
         IfthenpayPaymentReturn $ifthenpayPaymentReturn,
         IfthenpayLogger $logger,
         RepositoryFactory $repositoryFactory
-    )
-	{
+    ) {
         $this->gateway = $gateway;
         $this->ifthenpayPaymentReturn = $ifthenpayPaymentReturn;
         $this->logger = $logger;
         $this->repositoryFactory = $repositoryFactory;
-	}
+    }
 
     public function execute(Observer $observer)
     {
         $payment = $observer->getEvent()->getPayment();
         $paymentMethod = $payment->getMethod();
+
+        $isIfthenpayMethod = $this->gateway->checkIfthenpayPaymentMethod($paymentMethod) && $paymentMethod !== Gateway::CCARD && empty($ifthenpayPayment);
+
+        if (!$isIfthenpayMethod) {
+            return true;
+        }
+
         $order = $payment->getOrder();
 
         $ifthenpayPayment = $this->repositoryFactory->setType($paymentMethod)->build()->getByOrderId($order->getIncrementId())->getData();
         try {
-            if ($this->gateway->checkIfthenpayPaymentMethod($paymentMethod) && $paymentMethod !== Gateway::CCARD && empty($ifthenpayPayment)) {
+
+            if ($isIfthenpayMethod) {
 
                 switch ($paymentMethod) {
                     case Gateway::MULTIBANCO:
