2e5a4e88821e3c80acf94420826b03ab634dd42d
josesoaresif <josesoares@ifthenpay.com>
2024-01-17 16:24:43 +0000

    release: module update from ifthenpay/dev_magento_2.4_aio (https://github.com/ifthenpay/dev_magento_2.4_aio/commit/3e32ccd2d17065b1c53838e3bfc4017b389a855d)

---
+++ scripts/translations/translations.php
@@ -0,0 +1,11 @@
+<?php
+
+// TODO: future work, translations may be slightly easier to maintain if I create an array like the one below and then
+// just run a script to create the respective csv files.
+
+$t = [];
+
+$t['Select a Cofidis Pay key'] = [
+    "us" => "Select a Cofidis Pay key",
+    "pt" => "Selecione uma chave Cofidis Pay"
+];
